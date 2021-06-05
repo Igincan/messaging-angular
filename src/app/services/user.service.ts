a065b5f604214b36959064dc4c3b44c98380776d
Igincan <igino97@gmail.com>
2021-06-06 00:53:37 +0200

    little changes in user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -21,22 +21,25 @@ export class UserService {
   }
 
   async login(credentials: CredentialsForm): Promise<boolean> {
-    await this._http.post<MessageResponse>("/api/login", credentials).toPromise();
-    this.isLogged = true;
-    this.username = credentials.username;
-    return Promise.resolve(true);
+    if (credentials.username && credentials.password) {
+      await this._http.post<string>("/api/login", credentials).toPromise();
+      this.isLogged = true;
+      this.username = credentials.username;
+      return Promise.resolve(true);
+    } else {
+      return Promise.resolve(false);
+    }
   }
 
   async check(): Promise<boolean> {
     this.isLogged = await this._http.get<boolean>("api/isLogged").toPromise();
-    this.username = (await this._http.get<User>("/api/logged").toPromise()).username;
+    this.username = await this._http.get<string>("/api/logged").toPromise();
     return Promise.resolve(true);
   }
 
   async logout(): Promise<boolean> {
-    await this._http.get<MessageResponse>("/api/logout").toPromise();
+    await this._http.get<string>("/api/logout").toPromise();
     this.isLogged = false;
-    this.username = undefined;
     return Promise.resolve(true);
   }
 }
